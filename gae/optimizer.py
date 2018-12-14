ac1dcb71ba97bd57265597baa5c31af375f79995
MysteryVaibhav <mysteryvaibhav@gmail.com>
2018-12-14 02:34:06 -0500

    Added positive weighting to the bce loss calculation

--- gae/optimizer.py
+++ gae/optimizer.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 def loss_function(preds, labels, mu, logvar, n_nodes, norm, pos_weight):
-    cost = norm * F.binary_cross_entropy_with_logits(preds, labels, pos_weight=pos_weight)
+    cost = norm * F.binary_cross_entropy_with_logits(preds, labels, pos_weight=labels * pos_weight)
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
